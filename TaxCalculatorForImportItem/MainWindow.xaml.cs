cfdccc722a76ede1144330edb1095b6c41b18797
Xzib <syedzohaibali94@gmail.com>
2018-12-27 17:29:39 +0500

    edited AssesedValue

--- TaxCalculatorForImportItem/MainWindow.xaml.cs
+++ TaxCalculatorForImportItem/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace TaxCalculatorForImportItem
 {
@@ -35,70 +25,60 @@ namespace TaxCalculatorForImportItem
         private string GetNewValue()
         {
             //Assesed Value Calculation
-            int assesedValue;
-            bool inputCost = int.TryParse(this.ItemCost.Text,out assesedValue);
 
-
+            float assesedValue = float.Parse(this.ItemCost.Text);
             float currencyTolerance = float.Parse(this.CurrencyTolerance.Text);
-
-
             float insuranceValue = float.Parse(this.Insurance.Text);
             
-
-
             string currType =this.CurrencySelection.SelectedItem.ToString();
+
             if (currType.Contains("USD"))
             {
-
-                int usd = (Int32)CurrencyValue.USD;
-                int value = assesedValue * usd;
-                float Nvalue =value+value * (currencyTolerance/100);
-                float result =Nvalue+Nvalue * (insuranceValue / 100);
-                return (String)(result).ToString();
+                float usd = (Int32)CurrencyValue.USD;
+                float value = assesedValue * usd;
+                value = value + value * (currencyTolerance / 100);
+                value = value + value * (insuranceValue / 100);
+                return (String)(value).ToString();
             }
 
             else if (currType.Contains("EUR"))
             {
-
-                int eur = (Int32)CurrencyValue.EUR;
-                int value = assesedValue * eur;
-                float Nvalue = value + value * (currencyTolerance / 100);
-                float result = Nvalue + Nvalue * (insuranceValue / 100);
-                return (String)(result).ToString();
+                float eur = (Int32)CurrencyValue.EUR;
+                float value = assesedValue * eur;
+                value = value + value * (currencyTolerance / 100);
+                value = value + value * (insuranceValue / 100);
+                return (String)(value).ToString();
             }
 
             else if (currType.Contains("GBP"))
             {
 
-                int pound = (Int32)CurrencyValue.GBP;
-                int value = assesedValue * pound;
-                float Nvalue = value + value * (currencyTolerance / 100);
-                float result = Nvalue + Nvalue * (insuranceValue / 100);
-                return (String)(result).ToString();
+                float pound = (Int32)CurrencyValue.GBP;
+                float value = assesedValue * pound;
+                value = value + value * (currencyTolerance / 100);
+                value = value + value * (insuranceValue / 100);
+                return (String)(value).ToString();
             }
 
             else if (currType.Contains("YUAN"))
             {
-
-                int yuan = (Int32)CurrencyValue.YUAN;
-                int value = assesedValue * yuan;
-                float Nvalue = value + value * (currencyTolerance / 100);
-                float result = Nvalue + Nvalue * (insuranceValue / 100);
-                return (String)(result).ToString();
+                float yuan = (Int32)CurrencyValue.YUAN;
+                float value = assesedValue * yuan;
+                value = value + value * (currencyTolerance / 100);
+                value = value + value * (insuranceValue / 100);
+                return (String)(value).ToString();
             }
 
             else if (currType.Contains("Rs"))
             {
-
-                int rs = (Int32)CurrencyValue.RS;
-                int value = assesedValue * rs;
-                float Nvalue = value + value * (currencyTolerance / 100);
-                float result = Nvalue + Nvalue * (insuranceValue / 100);
-                return (String)(result).ToString();
+                float rs = (Int32)CurrencyValue.RS;
+                float value = assesedValue * rs;
+                value = value + value * (currencyTolerance / 100);
+                value = value + value * (insuranceValue / 100);
+                return (String)(value).ToString();
             }
             else
                 return "problem";
-
         }
     }
 
